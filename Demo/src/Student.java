fd50d84ee0f41250c91451d8bb3e3d9e24f7456b
Administrator <Administrator@192.168.0.103>
2017-05-24 09:35:30 +0800

    第一次提交

--- Demo/src/Student.java
+++ Demo/src/Student.java
@@ -1,6 +1,17 @@
+import java.util.Iterator;
+
 
 public class Student {
 public static void main(String[] args) {
-	System.out.println("111");
+	
+	String[] a = {"1","2","3"};
+	
+for (int i = 0; i < a.length; i++) {
+	if(a[i].equals("2")){
+		System.out.println("a[i]="+a[i]);
+		break;
+	}
+}
+	
 }
 }
